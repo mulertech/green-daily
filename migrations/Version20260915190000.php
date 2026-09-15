d86b881816f5533c2ab7ba9562c9bdc6aa554771
Sébastien Muler <sebastien.muler@mulertech.net>
2026-09-15 18:32:51 +0200

    chore(migration): test for docker-prod

--- migrations/Version20260915190000.php
+++ migrations/Version20260915190000.php
@@ -7,16 +7,16 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20260915180000 extends AbstractMigration
+final class Version20260915190000 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Essai docker-prod : migration qui échoue';
+        return 'Essai docker-prod : migration lente, pour interrompre la bascule';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('SELECT 1 FROM essai_table_absente');
+        $this->addSql('SELECT pg_sleep(30)');
     }
 
     public function down(Schema $schema): void
